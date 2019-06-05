3db839b38155834739e23ab1b11fcfa1b69c9216
yochaitamir <yochai1911@gmail.com>
2019-06-05 18:15:04 +0300

    Initial commit

--- messages/settings.py
+++ messages/settings.py
@@ -128,7 +128,7 @@ STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
