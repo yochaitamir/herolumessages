d4da37c41052ec2d67e10e0f83a29835f1c2d937
yochaitamir <yochai1911@gmail.com>
2019-06-05 21:10:55 +0300

    Initial commit

--- messagetask/models.py
+++ messagetask/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from datetime import datetime,date
 # Create your models here.
-class Messages((models.Model)):
-    sender=models.CharField ( max_length=200, null=True, blank=True) 
-    reciever=models.CharField ( max_length=200, null=True, blank=True) 
-    message=models.CharField ( max_length=200, null=True, blank=True) 
-    subject=models.CharField ( max_length=200, null=True, blank=True)
-    creation_date=models.DateTimeField(default=datetime.now)
-    read=models.BooleanField(("read"))
+# class Messages((models.Model)):
+#     sender=models.CharField ( max_length=200, null=True, blank=True) 
+#     reciever=models.CharField ( max_length=200, null=True, blank=True) 
+#     message=models.CharField ( max_length=200, null=True, blank=True) 
+#     subject=models.CharField ( max_length=200, null=True, blank=True)
+#     creation_date=models.DateTimeField(default=datetime.now)
+#     read=models.BooleanField(("read"))
